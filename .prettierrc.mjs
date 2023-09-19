cb4f5ec38a24bc9f96ba0fdeec1b048bbdfcebfc
Robert Csaszar <robert.csaszar@dowdian.com>
2023-09-19 19:08:07 +0300

    ⚡️ add compression & additional prettier stuff

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,7 +1,11 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-  tailwindConfig: "./tailwind.config.cjs",
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: "es5",
+  semi: true,
+  printWidth: 100,
   overrides: [
     {
       files: "*.astro",
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import { defineConfig } from "astro/config";
+import { siteUrl } from "./src/data/site.json";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
 export default defineConfig({
-   site: 'https://lexingtonthemes.com',
-  integrations: [tailwind(),  sitemap()]
+  site: siteUrl,
+  integrations: [tailwind(), sitemap(), compress()],
 });
